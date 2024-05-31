ce967bf1e4891e9a83978699092837ac1768ebf8
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2024-05-30 22:41:23 -0300

    feat(treino): find treino por id

--- src/main/java/com/example/traintrax/domain/treino/TreinoRepository.java
+++ src/main/java/com/example/traintrax/domain/treino/TreinoRepository.java
@@ -1,6 +1,8 @@
 package com.example.traintrax.domain.treino;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface TreinoRepository extends JpaRepository<Treino, Long> {
+    Treino findAllById(Long idTreino);
 }
