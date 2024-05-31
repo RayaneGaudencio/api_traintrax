7aa0097b498445c96e9379057de9cdbed2f100f8
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2024-05-30 23:20:14 -0300

    fix(exercicio): anvalidation id null

--- src/main/java/com/example/traintrax/domain/exercicio/DadosCadastroExercicio.java
+++ src/main/java/com/example/traintrax/domain/exercicio/DadosCadastroExercicio.java
@@ -1,9 +1,10 @@
 package com.example.traintrax.domain.exercicio;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record DadosCadastroExercicio(
-
+        @NotNull
         Long idTreino,
 
         @NotBlank
