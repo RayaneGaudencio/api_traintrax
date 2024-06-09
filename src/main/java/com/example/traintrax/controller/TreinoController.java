a1de4aef7f0c620f0a65af5368ffbe5db5238226
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2024-06-08 21:39:46 -0300

    feat(treino): endpoint listar treinos

--- src/main/java/com/example/traintrax/controller/TreinoController.java
+++ src/main/java/com/example/traintrax/controller/TreinoController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/treino")
@@ -41,6 +40,17 @@ public class TreinoController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocorreu um erro ao editar os dados do treino.");
+        }
+    }
+
+    @GetMapping("/lista")
+    @Transactional
+    public ResponseEntity listarTreinos() {
+        try {
+            List<Treino> treinos = treinoRepository.findAll();
+            return ResponseEntity.ok(treinos);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
 }
