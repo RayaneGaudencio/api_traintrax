09a320318c903277ba81d43b1bdb4f2496c99090
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2024-06-09 16:38:22 -0300

    refactor(treino): padrão do dado CamelCase

--- src/main/java/com/example/traintrax/domain/treino/DadosTreino.java
+++ src/main/java/com/example/traintrax/domain/treino/DadosTreino.java
@@ -3,7 +3,7 @@ package com.example.traintrax.domain.treino;
 public record DadosTreino(
         Long id,
         String nome,
-        String dia_semana
+        String diaSemana
 ) {
     public DadosTreino(Treino treino) {
         this(treino.getId(), treino.getNome(), treino.getDiaSemana());
